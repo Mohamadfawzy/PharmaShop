71df05e75738959db89584aea911a963c0537242
Mohamed Fawzy <47891524+Mohamadfawzy@users.noreply.github.com>
2025-07-15 18:05:35 +0300

    Prescriptions

--- PharmaShopScript4.sql
+++ PharmaShopScript4.sql
@@ -1,5 +1,13 @@
-﻿
+﻿ 
+ USE master;
+ IF NOT EXISTS(SELECT name FROM sys.databases WHERE name = 'pharma_shope_db')
+ BEGIN
+     CREATE DATABASE pharma_shope_db COLLATE Arabic_100_CI_AS_KS_WS_SC_UTF8;
+ END
+ GO
 
+USE pharma_shope_db;
+Go
 CREATE TABLE Pharmacies (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name NVARCHAR(200) NOT NULL,                
@@ -14,6 +22,8 @@ CREATE TABLE Pharmacies (
     IsActive BIT NOT NULL DEFAULT 1            
 );
 
+GO
+
 CREATE TABLE Users (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Username NVARCHAR(100) NOT NULL UNIQUE,
@@ -26,11 +36,15 @@ CREATE TABLE Users (
     UpdatedAt DATETIME NULL
 );
 
+GO
+
 CREATE TABLE Roles (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(50) NOT NULL UNIQUE, -- أمثلة: Customer, Admin, Doctor, DeliveryRep
     Description NVARCHAR(200) NULL
 );
+
+GO
 
 CREATE TABLE UserRoles (
     UserId INT NOT NULL,
@@ -42,6 +56,19 @@ CREATE TABLE UserRoles (
     -- ملاحظة: لا يوجد FK مباشر على EntityId لأنه يمكن أن يشير لجداول مختلفة
 );
 
+GO
+
+CREATE TABLE Admins (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    PharmacyId INT NOT NULL, -- تشير إلى الصيدلية المالكة (مستقبلاً عند التوسع)
+    FullName NVARCHAR(200) NOT NULL,
+    Email NVARCHAR(200) NOT NULL,
+    PasswordHash NVARCHAR(500) NOT NULL,
+    Role NVARCHAR(100) NOT NULL, -- Admin, Support, Viewer
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE()
+);
+
+GO
 
 CREATE TABLE Customers (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -70,6 +97,8 @@ CREATE TABLE Customers (
     CONSTRAINT FK_Customers_Pharmacies FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(Id)
 );
 
+GO
+
 CREATE TABLE CustomerAddresses (
     Id INT PRIMARY KEY IDENTITY(1,1),
     CustomerId INT NOT NULL,
@@ -85,6 +114,25 @@ CREATE TABLE CustomerAddresses (
     CONSTRAINT FK_CustomerAddresses_Customer FOREIGN KEY (CustomerId) REFERENCES Customers(Id)
 );
 
+GO
+
+-- الصيدلي
+CREATE TABLE Pharmacists (
+    Id INT PRIMARY KEY IDENTITY,
+    FullName NVARCHAR(100),
+    Specialty NVARCHAR(100),
+    UserId INT UNIQUE NULL,
+    FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+
+
+GO
+
+--=======================================
+--=======================================
+--=======================================
+--=======================================
+--=======================================
 CREATE TABLE Categories (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(200) NOT NULL,
@@ -96,7 +144,9 @@ CREATE TABLE Categories (
 	ParentCategoryId INT NULL, -- ++
 );
 
-CREATE TABLE Sub1categories (
+GO
+
+CREATE TABLE Sub1Categories (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(200) NOT NULL,
     Description NVARCHAR(MAX) NULL,
@@ -105,10 +155,12 @@ CREATE TABLE Sub1categories (
     IsActive BIT NOT NULL DEFAULT 1,
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
     UpdatedAt DATETIME NULL,
-    CONSTRAINT FK_Subcategories_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+    CONSTRAINT FK_Sub1categories_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
 );
 
-CREATE TABLE Sub2categories (
+GO
+
+CREATE TABLE Sub2Categories (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(200) NOT NULL,
     Description NVARCHAR(MAX) NULL,
@@ -117,8 +169,10 @@ CREATE TABLE Sub2categories (
     IsActive BIT NOT NULL DEFAULT 1,
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
     UpdatedAt DATETIME NULL,
-    CONSTRAINT FK_Subcategories_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+    CONSTRAINT FK_Sub2categories_CategoryId FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
 );
+
+GO
 
 CREATE TABLE Products (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -139,8 +193,11 @@ CREATE TABLE Products (
     CreatedBy NVARCHAR(100) NULL,
     IsActive BIT NOT NULL DEFAULT 1,
 
-	CONSTRAINT FK_Products_CategoryId FOREIGN KEY (SubCategoryId) REFERENCES Sub2categories(Id)
+	CONSTRAINT FK_Products_CategoryId FOREIGN KEY (SubCategoryId)
+		REFERENCES Sub2Categories(Id)
 );
+
+GO
 
 CREATE TABLE ProductImages (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -151,8 +208,10 @@ CREATE TABLE ProductImages (
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
     CONSTRAINT FK_ProductImages_Products_ID FOREIGN KEY (ProductId) 
-	REFERENCES Products(Id)ON DELETE CASCADE ON UPDATE CASCADE,
+		REFERENCES Products(Id)ON DELETE CASCADE ON UPDATE CASCADE,
 );
+
+GO
 
 CREATE TABLE Tags (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -167,23 +226,90 @@ CREATE TABLE ProductTags (
     FOREIGN KEY (ProductId) REFERENCES Products(Id),
     FOREIGN KEY (TagId) REFERENCES Tags(Id)
 );
+-- =======================================Prescriptions===========================================
+-- ========================================================================================================
+-- الروشته
+
+GO
+
+CREATE TABLE Prescriptions (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    
+    CustomerId INT NOT NULL,
+    PharmacyId INT NOT NULL,
+
+    Notes NVARCHAR(500) NULL,                      -- ملاحظات يكتبها العميل
+    Status NVARCHAR(50) NOT NULL DEFAULT 'Pending',-- Pending, Approved, Rejected
+
+    ReviewedByUserId INT NULL,                     -- من راجع الروشتة
+    ReviewedAt DATETIME NULL,                      -- متى تمت المراجعة
+
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    FOREIGN KEY (CustomerId) REFERENCES Customers(Id),
+    FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(Id),
+    FOREIGN KEY (ReviewedByUserId) REFERENCES Users(Id)
+);
+
+GO
+
+
+CREATE TABLE PrescriptionImages (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    PrescriptionId INT NOT NULL,
+    FileUrl NVARCHAR(500) NOT NULL,     -- رابط الصورة المرفوعة
+    SortOrder INT NULL,                 -- لترتيب الصور عند العرض
+
+    UploadedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    FOREIGN KEY (PrescriptionId) REFERENCES Prescriptions(Id)
+);
+
+GO
+
+
+CREATE TABLE PrescriptionItems (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    PrescriptionId INT NOT NULL,
+    ProductId INT NOT NULL,
+    Quantity INT NOT NULL,
+
+    FOREIGN KEY (PrescriptionId) REFERENCES Prescriptions(Id),
+    FOREIGN KEY (ProductId) REFERENCES Products(Id)
+);
+
+GO
+
 
 CREATE TABLE SalesHeader (
     Id int PRIMARY KEY IDENTITY(1,1),
-    InvoiceNumber NVARCHAR(50) NOT NULL,
     CustomerId INT NULL,
+    PrescriptionId INT NULL, -- إذا كان الطلب نتيجة لروشتة
+
+	IsFromPrescription BIT NOT NULL DEFAULT 0, 
+	-- TRUE If the request is a result of a prescription
+    InvoiceNumber NVARCHAR(50) NOT NULL,
     OrderDate DATETIME NOT NULL DEFAULT GETDATE(),
     TotalAmount DECIMAL(18, 2) NOT NULL,
     Discount DECIMAL(18, 2) NOT NULL DEFAULT 0,
     NetAmount AS (TotalAmount - Discount) PERSISTED,
     PaymentMethod NVARCHAR(50) NOT NULL,
+	IsFreeShipping BIT NOT NULL DEFAULT 0, -- هل الطلب يحتوي على شحن مجاني
     Notes NVARCHAR(MAX) NULL,
-    Status NVARCHAR(20) NOT NULL DEFAULT 'Pending', --Pending, Processing, Completed, Returned, PartiallyReturned, Cancelled
+    Status NVARCHAR(50) NOT NULL DEFAULT 'Pending', --Pending, ConfirmedByCustomer,RejectedByCustomer, Processing, Completed, Returned, PartiallyReturned, Cancelled, Delivered
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
     CONSTRAINT FK_SalesHeader_Customers FOREIGN KEY (CustomerId)
-        REFERENCES Customers(Id)
+        REFERENCES Customers(Id),
+
+    CONSTRAINT FK_SalesHeader_Prescriptions FOREIGN KEY (PrescriptionId)
+		REFERENCES Prescriptions(Id)
 );
+
+GO
+
 
 CREATE TABLE SalesDetails (
     Id INT PRIMARY KEY IDENTITY(1,1),
@@ -201,6 +327,9 @@ CREATE TABLE SalesDetails (
     CONSTRAINT FK_SalesDetails_Products FOREIGN KEY (ProductId)
         REFERENCES Products(Id)
 );
+
+GO
+
 
 CREATE TABLE SalesHeaderReturns (
     Id INT PRIMARY KEY IDENTITY(1,1),
@@ -225,6 +354,9 @@ CREATE TABLE SalesHeaderReturns (
      CONSTRAINT FK_SalesHeaderReturns_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(Id),
      CONSTRAINT FK_SalesHeaderReturns_Pharmacies FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(Id)
 );
+
+GO
+
 
 CREATE TABLE SalesDetailsReturns (
     Id INT PRIMARY KEY IDENTITY(1,1),
@@ -262,37 +394,145 @@ CREATE TABLE SalesDetailsReturns (
 --);
 
 -- الوصفات الطبية
-CREATE TABLE Prescriptions(
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    SalesHeaderId INT NULL,  -- يمكن أن تكون الروشتة محفوظة بدون طلب بعد
-    ImageUrl NVARCHAR(500) NOT NULL,
-    UploadedAt DATETIME NOT NULL DEFAULT GETDATE(),
-    Notes NVARCHAR(500) NULL,
 
-    CONSTRAINT FK_Prescriptions_SalesHeader 
-        FOREIGN KEY (SalesHeaderId)
-        REFERENCES SalesHeader(Id)
+
+
+
+--CREATE TABLE PromotionTypes (
+--    Id INT IDENTITY(1,1) PRIMARY KEY,
+--    Code NVARCHAR(50) NOT NULL UNIQUE, -- مثال: 'Discount', 'FreeShipping', etc.
+--    Name NVARCHAR(150) NOT NULL        -- يمكن استخدامه للعرض بالعربية أو الإنجليزية
+--);
+
+GO
+
+
+-- ProductsOffer
+CREATE TABLE Promotions (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    PharmacyId INT NOT NULL,                        -- العرض مرتبط بأي صيدلية
+
+    Title NVARCHAR(200) NOT NULL,                   -- عنوان العرض الظاهر للمستخدم
+    Description NVARCHAR(MAX) NULL,                 -- تفاصيل العرض
+    
+    PromoType NVARCHAR(50) NOT NULL CHECK (PromoType IN ('PercentDiscount','FixedDiscount','FreeShipping', 'ExtraPoints', 'BuyXGetY', 'FreeItem')),
+        
+	DiscountValue DECIMAL(18,2) NULL,				-- قيمة الخصم (نسبة أو مبلغ ثابت)
+    ExtraPoints INT NULL,                           -- عدد النقاط الإضافية إن وُجد
+    FreeProductId INT NULL,                         -- المنتج المجاني إن وُجد
+
+    MinPurchaseAmount DECIMAL(18,2) NULL,           -- الحد الأدنى للطلب لتفعيل العرض
+    MaxUsagePerCustomer INT NULL,                   -- أقصى عدد مرات للمستخدم الواحد
+    MaxUsageOverall INT NULL,                       -- أقصى عدد مرات للعرض بشكل عام
+
+    StartsAt DATETIME NOT NULL,
+    EndsAt DATETIME NOT NULL,
+
+    IsActive BIT NOT NULL DEFAULT 1,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT FK_Promotions_Pharmacies FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(Id)
 );
 
-CREATE TABLE ProductsOffer (
-    Id INT PRIMARY KEY IDENTITY(1,1),
+
+GO
+
+
+CREATE TABLE PromotionProducts (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    PromotionId INT NOT NULL,
     ProductId INT NOT NULL,
-    Title NVARCHAR(200) NOT NULL,
-    Description NVARCHAR(1000) NULL,
-    DiscountPercentage DECIMAL(5,2) NOT NULL,  -- مثلاً 10.00 تعني خصم 10%
+
+    CONSTRAINT FK_PromotionProducts_Promotions FOREIGN KEY (PromotionId) REFERENCES Promotions(Id),
+    CONSTRAINT FK_PromotionProducts_Products FOREIGN KEY (ProductId) REFERENCES Products(Id)
+);
+
+
+CREATE TABLE PromotionCategories (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    PromotionId INT NOT NULL,
+    CategoryId INT NOT NULL,
+
+    CONSTRAINT FK_PromotionCategories_Promotions FOREIGN KEY (PromotionId) REFERENCES Promotions(Id),
+    CONSTRAINT FK_PromotionCategories_Categories FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+);
+
+GO
+
+
+CREATE TABLE PromotionUsage (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    PromotionId INT NOT NULL,
+    UserId INT NOT NULL,
+    UsageDate DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT FK_PromotionUsage_Promotions FOREIGN KEY (PromotionId) REFERENCES Promotions(Id),
+    CONSTRAINT FK_PromotionUsage_Users FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+
+
+
+GO
+
+
+--=========================================
+CREATE TABLE PromoCodes (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+    Code NVARCHAR(50) NOT NULL UNIQUE,         -- كود الخصم نفسه
+
+    Description NVARCHAR(500) NULL,
+
+    PromoType NVARCHAR(50) NOT NULL CHECK (PromoType IN ('Discount', 'ExtraPoints', 'FreeShipping', 'GiftProduct')),
+
+    DiscountType NVARCHAR(20) NULL CHECK (DiscountType IN ('Fixed', 'Percent')),
+    DiscountValue DECIMAL(18,2) NULL,
+
+    ExtraPointsValue INT NULL,
+
+    MaxUsageCount INT NULL,                     -- الحد الأقصى لاستخدام الكود عمومًا
+    UsedCount INT NOT NULL DEFAULT 0,           -- كم مرة تم استخدامه حتى الآن
+
+    IsSingleUsePerCustomer BIT NOT NULL DEFAULT 0, -- هل يمكن استخدامه مرة واحدة فقط لكل عميل
+
+    CustomerId INT NULL,                        -- لو مخصص لعميل معين
+    ProductId INT NULL,                         -- لو مرتبط بمنتج معين
+    CategoryId INT NULL,                        -- لو مرتبط بتصنيف
+
     StartDate DATETIME NOT NULL,
     EndDate DATETIME NOT NULL,
 
-    CONSTRAINT FK_ProductsOffer_Product 
-        FOREIGN KEY (ProductId) 
-        REFERENCES Products(Id)
+    IsActive BIT NOT NULL DEFAULT 1,
+
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT FK_PromoCodes_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(Id),
+    CONSTRAINT FK_PromoCodes_Products FOREIGN KEY (ProductId) REFERENCES Products(Id),
+    CONSTRAINT FK_PromoCodes_Categories FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
 );
 
+GO
+
+
+CREATE TABLE PromoCodeUsages (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    PromoCodeId INT NOT NULL,
+    CustomerId INT NOT NULL,
+    UsedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT FK_PromoCodeUsages_PromoCodes FOREIGN KEY (PromoCodeId) REFERENCES PromoCodes(Id),
+    CONSTRAINT FK_PromoCodeUsages_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(Id)
+);
+
+GO
+
 
 --=========================================
 --=========================================
 --=========================================
---=========================================
+
+GO
+
 
 CREATE TABLE CustomerPointsHistory (
     Id INT IDENTITY(1,1) PRIMARY KEY,
@@ -315,6 +555,7 @@ CREATE TABLE CustomerPointsHistory (
     CONSTRAINT FK_CustomerPointsHistory_Pharmacies FOREIGN KEY (PharmacyId) REFERENCES Pharmacies(Id)
 );
 
+GO
 
 
 
@@ -327,77 +568,71 @@ CREATE TABLE CustomerPointsHistory (
 
 
 
-CREATE TABLE Reviews (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    CustomerId INT NOT NULL,
-    ProductId INT NOT NULL,
-    Rating INT NOT NULL CHECK (Rating BETWEEN 1 AND 5),
-    Comment NVARCHAR(1000) NULL,
-    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
-    CONSTRAINT FK_Reviews_Customer 
-        FOREIGN KEY (CustomerId) 
-        REFERENCES Customers(Id),
 
-    CONSTRAINT FK_Reviews_Product 
-        FOREIGN KEY (ProductId) 
-        REFERENCES Products(Id)
-);
+--CREATE TABLE Reviews (
+--    Id INT PRIMARY KEY IDENTITY(1,1),
+--    CustomerId INT NOT NULL,
+--    ProductId INT NOT NULL,
+--    Rating INT NOT NULL CHECK (Rating BETWEEN 1 AND 5),
+--    Comment NVARCHAR(1000) NULL,
+--    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
-CREATE TABLE Chats (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    CustomerId INT NOT NULL,
-    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+--    CONSTRAINT FK_Reviews_Customer 
+--        FOREIGN KEY (CustomerId) 
+--        REFERENCES Customers(Id),
 
-    CONSTRAINT FK_Chats_Customer 
-        FOREIGN KEY (CustomerId) 
-        REFERENCES Customers(Id)
-);
-CREATE TABLE Messages (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    ChatId INT NOT NULL,
-    SenderType NVARCHAR(50) NOT NULL,  -- Admin / Customer
-    MessageText NVARCHAR(2000) NOT NULL,
-    SentAt DATETIME NOT NULL DEFAULT GETDATE(),
+--    CONSTRAINT FK_Reviews_Product 
+--        FOREIGN KEY (ProductId) 
+--        REFERENCES Products(Id)
+--);
 
-    CONSTRAINT FK_Messages_Chat 
-        FOREIGN KEY (ChatId) 
-        REFERENCES Chats(Id)
-);
-CREATE TABLE Notifications (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    CustomerId INT NOT NULL,
-    Title NVARCHAR(200) NOT NULL,
-    Body NVARCHAR(1000) NOT NULL,
-    IsRead BIT NOT NULL DEFAULT 0,
-    SentAt DATETIME NOT NULL DEFAULT GETDATE(),
+--CREATE TABLE Chats (
+--    Id INT PRIMARY KEY IDENTITY(1,1),
+--    CustomerId INT NOT NULL,
+--    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
-    CONSTRAINT FK_Notifications_Customer 
-        FOREIGN KEY (CustomerId) 
-        REFERENCES Customers(Id)
-);
-CREATE TABLE Admins (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    PharmacyId INT NOT NULL, -- تشير إلى الصيدلية المالكة (مستقبلاً عند التوسع)
-    FullName NVARCHAR(200) NOT NULL,
-    Email NVARCHAR(200) NOT NULL,
-    PasswordHash NVARCHAR(500) NOT NULL,
-    Role NVARCHAR(100) NOT NULL, -- Admin, Support, Viewer
-    CreatedAt DATETIME NOT NULL DEFAULT GETDATE()
-);
-CREATE TABLE AuditLogs (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    AdminId INT NULL,
-    Action NVARCHAR(500) NOT NULL,
-    TableName NVARCHAR(100) NULL,
-    RecordId INT NULL,
-    Timestamp DATETIME NOT NULL DEFAULT GETDATE(),
-    Notes NVARCHAR(1000) NULL,
+--    CONSTRAINT FK_Chats_Customer 
+--        FOREIGN KEY (CustomerId) 
+--        REFERENCES Customers(Id)
+--);
+--CREATE TABLE Messages (
+--    Id INT PRIMARY KEY IDENTITY(1,1),
+--    ChatId INT NOT NULL,
+--    SenderType NVARCHAR(50) NOT NULL,  -- Admin / Customer
+--    MessageText NVARCHAR(2000) NOT NULL,
+--    SentAt DATETIME NOT NULL DEFAULT GETDATE(),
 
-    CONSTRAINT FK_AuditLogs_Admin 
-        FOREIGN KEY (AdminId) 
-        REFERENCES Admins(Id)
-);
+--    CONSTRAINT FK_Messages_Chat 
+--        FOREIGN KEY (ChatId) 
+--        REFERENCES Chats(Id)
+--);
+--CREATE TABLE Notifications (
+--    Id INT PRIMARY KEY IDENTITY(1,1),
+--    CustomerId INT NOT NULL,
+--    Title NVARCHAR(200) NOT NULL,
+--    Body NVARCHAR(1000) NOT NULL,
+--    IsRead BIT NOT NULL DEFAULT 0,
+--    SentAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+--    CONSTRAINT FK_Notifications_Customer 
+--        FOREIGN KEY (CustomerId) 
+--        REFERENCES Customers(Id)
+--);
+
+--CREATE TABLE AuditLogs (
+--    Id INT PRIMARY KEY IDENTITY(1,1),
+--    AdminId INT NULL,
+--    Action NVARCHAR(500) NOT NULL,
+--    TableName NVARCHAR(100) NULL,
+--    RecordId INT NULL,
+--    Timestamp DATETIME NOT NULL DEFAULT GETDATE(),
+--    Notes NVARCHAR(1000) NULL,
+
+--    CONSTRAINT FK_AuditLogs_Admin 
+--        FOREIGN KEY (AdminId) 
+--        REFERENCES Admins(Id)
+--);
 
 
 
