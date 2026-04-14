f1d65890fdbfe9b00c2d9842de7209505d13aa49
Mohamed Fawzy <47891524+Mohamadfawzy@users.noreply.github.com>
2026-04-14 15:13:43 +0200

    end preview qustions

--- Repository/CartRepository.cs
+++ Repository/CartRepository.cs
@@ -3,11 +3,6 @@ using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Shared.Enums.Cart;
 using Shared.Models.Dtos.Cart;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository;
 
